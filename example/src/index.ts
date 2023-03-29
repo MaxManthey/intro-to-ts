e4dcd9eea2e21d93e900675ca945cc5bb1b91515
MaxManthey <maximilian.manthey@arconsis.com>
2023-03-29 11:43:27 +0200

    added part 4 exercise

--- example/src/index.ts
+++ example/src/index.ts
@@ -30,16 +30,48 @@ console.log(num3, num4)
 console.log(--num4)
 
 
-//Arrays []
+//Arrays [] contain multiple values of any type
+const arrNums = [1, 5, 7, 8, 9, 10]
+const arrStr = ["Hello", "Hi", "Howdy"]
+
+//Accessing Array with index starting at 0!
+console.log(arrNums[0])
+console.log(arrNums[4])
 
 
 //looping over arrays
+for(let i = 0; i < arrNums.length; i++) {
+  console.log(arrNums[i])
+}
+for(let i = 0; i < arrNums.length; i++) {
+  arrNums[i] = i
+}
+console.log(arrNums)
+
 
 
 //SCOPE {}
+// Stuff declared in curly braces {} can only be used in the braces
 
-
-
+// const myVar = 3
+function test() {
+  const myVar = 3
+}
+// console.log(myVar) doesnt work because it's inside of test()
+// counts not only for functions but everything else
+if(true) {
+  const myVar = 3
+}
+for(let i = 0; i < 3; i++) {
+  const myVar = 3
+}
+//but can be used in nested curly braces
+function nested() {
+  const myVar = 3
+  for(let i = 0; i < 2; ++i) {
+    console.log(myVar) // Works!
+  }
+}
 
 
 
