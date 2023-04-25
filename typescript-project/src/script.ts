ae220d235f7600065a4e6818bd52140a3813f7f9
MaxManthey <maximilian.manthey@arconsis.com>
2023-04-25 19:06:25 +0200

    finishing the series

--- typescript-project/src/script.ts
+++ typescript-project/src/script.ts
@@ -1,36 +1,32 @@
-// // let i = 0
-// // while(i < 5) {
-// //   console.log("Hello World", i)
-// //   i = i + 1
-// // }
+class superhero {
+  name: string
+  superpower: string
 
-// // for(let i = 0; i < 5; ++i) {
-// //  console.log("Hi there") 
-// // }
+  constructor (name: string, superpower: string) {
+    this.name = name
+    this.superpower = superpower
+  }
 
-// // let num1 = 5
-// // let num2 = num1--
-// // console.log(num1, num2)
+  sayHello () {
+    console.log ("Hello I am " + this.name)
+  }
+}
 
-// let num1 = 1
-// const num2: number = 2
-// // ...                      [0, 1, 2, 3, 4]
-// const arrNumber: number[] = [1, 2, 3, 4, 5]
-// const arrStrings: string[] = ["Hi", "Hello", "Howdy", "Hello", "Moin", "Hello"]
-// // console.log(arrNumber[4])
-// // console.log(arrNumber.length)
+const iSEVEN = new superhero ("iSEVEN", "Cleverness")
+const alvee = new superhero ("Alveeman" , "Sleeping all day")
 
-// // for(let i = arrStrings.length-2; i < arrStrings.length; i++) {
-// //   console.log(arrStrings[i], i)
-// // }
+iSEVEN.sayHello ()
+alvee.sayHello ()
 
-// // arrStrings[1] = "Hallo"
+class MarvelHero extends superhero {
+  isAvenger: boolean
 
-// let counter = 0
-// // console.log(arrStrings)
-// for(let i = arrStrings.length - 1; i >= 0; i--) {
-//   if(arrStrings[i] == "Hello") {
-//     ++counter
-//   }
-// }
-// console.log(counter)
+  constructor (name: string, superpower: string, isAvenger: boolean) {
+    super (name, superpower)
+    this.isAvenger = isAvenger
+  }
+}
+
+const Hawkeye = new MarvelHero ("Hawkeye", "sight", true)
+
+Hawkeye.sayHello()
